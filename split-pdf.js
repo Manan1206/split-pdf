26e804642584e169a05f2ab8e7928947a307e1cf
Manan <manan1206@gmail.com>
2020-07-31 14:58:11 +0530

    Removed comments/consoles and added folders to gitignore

--- split-pdf.js
+++ split-pdf.js
@@ -4,10 +4,10 @@ var path = require('path');
 const pdf = require('pdf-parse');
 
 // Max size of split pdf in bytes
-const MAX_FILE_SIZE = 4000000
+const MAX_FILE_SIZE = 20000000
 
 // Output Folder for split pdfs
-var outputFolder = path.join(__dirname, '/output');
+var outputFolder = path.join(__dirname, 'PDF_Files');
 
 // Input Directory of pdfs
 const directoryPath = path.join(__dirname, 'PDF_Files/');
@@ -19,24 +19,16 @@ fs.readdir(directoryPath, (err, files) => {
   }
   //listing all files using forEach
   files.forEach((file) => {
-
-    // Do whatever you want to do with the file
     let dataBuffer = fs.readFileSync('PDF_Files/' + file);
     pdf(dataBuffer).then(function (data) {
       this.pageCount = data.numpages
-      // number of pages
-      console.log("Data numpages : ", data.numpages);
       var stats = fs.statSync('PDF_Files/' + file)
       var fileSizeInBytes = stats["size"]
       if (fileSizeInBytes > MAX_FILE_SIZE) {
         var sourcePDF = path.join(directoryPath + file);
         filesCount = Math.ceil(fileSizeInBytes / MAX_FILE_SIZE);
         pagesPerFile = this.pageCount / filesCount
-        console.log(this.pageCount)
-        console.log(filesCount)
-        console.log(pagesPerFile)
-        fileName = file.replace('.pdf','')
-        console.log(fileName)
+        fileName = file.replace('.pdf', '')
         for (let i = 0; i < filesCount; i++) {
           var pdfWriter = hummus.createWriter(path.join(outputFolder, ` ${fileName} (${i}).pdf`));
           for (let j = 0; j < pagesPerFile; j++) {
